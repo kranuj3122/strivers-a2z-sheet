891f0d4afcb7a69d1e0d2523c2266d23b17ddb85
kranuj3122 <kumaranuj22799@gmail.com>
2025-03-06 13:01:02 +0530

    minor changes

--- step-01/lec02-logical-thinking/Patterns.java
+++ step-01/lec02-logical-thinking/Patterns.java
@@ -3,6 +3,6 @@ import java.io.*;
 
 public class Patterns {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!");
     }
 }
